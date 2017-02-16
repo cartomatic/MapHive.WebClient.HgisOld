dfaef29d70580ba55f86096e4522cf8d6bd8bddf
Dominik Mikiewicz <dominik@cartomatic.pl>
2017-02-16 22:45:56 +0100

    plug in new orgs controller

--- MapHive.WebClient.HgisOld/app/Application.js
+++ MapHive.WebClient.HgisOld/app/Application.js
@@ -12,27 +12,45 @@
             'MapHiveHgisOld.AppLauncher'
         ],
 
+        mixins: [
+            'mh.mixin.CallMeParent'
+        ],
+
         controllers: [
-            'MapHiveHgisOld.controller.Root',
+            'mh.controller.Root',
             'mh.controller.Auth',
+            'mh.controller.Organisation',
             'mh.controller.Splash'
         ],
 
         //here goes a custom app launcher class. If not overridden, it will default to mh.AppLauncher
-        //Note: in this case MapHiveHgisOld.AppLauncher just inherits from mh.AppLauncher and is used instead as
-        //this is needed  so the proper azzurra scss is brought in!
+        //Note: in this case MapHiveHgisOld.AppLauncher just inherits from mh.AppLauncher and is used instead
         appLauncher: 'MapHiveHgisOld.AppLauncher',
 
-        onLaunchApp: function(){
+        /**
+         * internal app launch procedure
+         * @param cfg
+         * @param cfg.userConfig
+         * @param cfg.orgCtx
+         */
+        internalAppLaunch: function(cfg){
 
             //first make sure the base does what's required!
-
-            //note: need to drilldown 2 levels, as in fact, this will be an instance of the app defined and launched in the app.js!
-            //this is needed, as the core funtionality is borrowed from the generic mh.Application
-            this.superclass.superclass.onLaunchApp.call(this);
+            this.callMeParent('internalAppLaunch', arguments);
 
             //start the external route watcher - this will watch changes advised by a parent, and pass own changes to a parent
             this.fireGlobal('root::watchexternalroutes', {host: true});
+
+
+            //finally force reroute to the specified route so the router kicks in and handles nav properly
+            //make sure though to postpone this a bit, so all the other stuff that can still be potentially configuring has some time to finish
+            Ext.defer(
+                function(){
+                    this.redirectTo(window.location.hash.replace('#', ''), true);
+                },
+                250,
+                this
+            );
         }
 
     });
